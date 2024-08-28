558d11f07e5b7cc6ce9fc7a0e6a2df8930ba8963
ethan-freeze <ethanfreeze@gmail.com>
2024-08-27 20:26:13 -0400

    Home page and registration form

---
+++ form-value-checker.js
@@ -0,0 +1,9 @@
+// User registration form popup handler
+
+function openForm() {
+    document.getElementById('form-container').style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById('form-container').style.display = "none";
+}
